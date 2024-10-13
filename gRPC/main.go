9ed30dd129990ca317bb7ea118762c83f7610d8c
hiroto60 <hiroto_yamazaki@schoo.jp>
2024-10-13 20:40:28 +0900

    gRPCの基本サーバー作成

---
+++ gRPC/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/hiroto1220/go-playground/gRPC/pb"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedHelloServiceServer
+}
+
+func (s *server) Hello(_ context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloResponse{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterHelloServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+
+	reflection.Register(s)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
