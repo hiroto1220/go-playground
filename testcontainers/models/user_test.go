cf859cdfa2d1c864a620aa8c51ef9046805c687b
hiroto1220 <tochihirototo.nya.49@gmail.com>
2025-04-16 15:49:19 +0900

    feat: moduleを利用しないテストの追加

--- testcontainers/models/user_test.go
+++ testcontainers/models/user_test.go
@@ -13,99 +13,42 @@ import (
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func TestUser_FetchUserByIDWithModule(t *testing.T) {
-	// Testconainersを使用してMySQLコンテナを準備
-	ctx := context.Background()
+type MysqlContainer struct {
+	mysqlContainer testcontainers.Container
+	URI            string
+}
 
+func setupMysqlContainerWithModule(ctx context.Context, schemaPath string) (*MysqlContainer, error) {
 	mysqlContainer, err := mysql.Run(ctx,
 		"mysql:8.0.36",
-		mysql.WithScripts("./schema.sql"),
+		mysql.WithScripts(schemaPath),
 
-		// データベース名やユーザー名、パスワードを指定することも可能
+		// github.com/testcontainers/testcontainers-go/modules/mysqlを使ってデータベース名やユーザー名、パスワードを指定することも可能
 		// mysql.WithDatabase("test"),
 		// mysql.WithUsername("user"),
 		// mysql.WithPassword("password"),
 	)
 
-	defer func() {
-		if err := testcontainers.TerminateContainer(mysqlContainer); err != nil {
-			t.Logf("failed to terminate container: %v", err)
-		}
-	}()
-
 	if err != nil {
-		t.Fatalf("failed to start MySQL container: %s", err)
+		return nil, fmt.Errorf("failed to start container: %s", err)
 	}
 
 	// MySQLに接続
 	dsn, err := mysqlContainer.ConnectionString(ctx)
 	if err != nil {
-		t.Fatalf("failed to get connection string: %s", err)
+		return nil, fmt.Errorf("failed to get connection string: %s", err)
 	}
 
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer db.Close()
-
-	r := NewUserRepository(db)
-
-	// テストデータを作成
-	type args struct {
-		userID string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *User
-		wantErr bool
-	}{
-		{
-			name: "userIDが1の時にデータベースからuserID 1に対応したユーザーを取得できる",
-			args: args{
-				userID: "1",
-			},
-			want: &User{
-				ID:    "1",
-				Name:  "yamada taro",
-				Email: "taro.yamada@example.com",
-			},
-			wantErr: false,
-		},
-		{
-			name: "データベースに登録していないuserIDを指定した時にエラーが発生する",
-			args: args{
-				userID: "999",
-			},
-			want:    nil,
-			wantErr: true,
-		},
+	mysqlC := &MysqlContainer{
+		mysqlContainer: mysqlContainer,
+		URI:            dsn,
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := r.FetchUserByID(tt.args.userID)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("User.FetchUserByID() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("User.FetchUserByID() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	return mysqlC, nil
 }
 
-func TestUser_FetchUserByID(t *testing.T) {
-	// Testconainersを使用してMySQLコンテナを準備
-	ctx := context.Background()
-
-	schemaPath, err := filepath.Abs("../models/schema.sql")
-	if err != nil {
-		t.Fatal(err)
-	}
-
+func setupMysqlContainer(ctx context.Context, schemaPath string) (*MysqlContainer, error) {
+	// MySQLコンテナの起動設定を作成（環境変数・ポート・初期SQLファイルの配置など）
 	req := testcontainers.ContainerRequest{
 		Image:        "mysql:8.0.36",
 		ExposedPorts: []string{"3306/tcp", "33060/tcp"},
@@ -125,57 +68,68 @@ func TestUser_FetchUserByID(t *testing.T) {
 		},
 	}
 
+	// コンテナ起動リクエストを作成
 	genericContainerReq := testcontainers.GenericContainerRequest{
 		ContainerRequest: req,
 		Started:          true,
 	}
 
+	// コンテナを起動
 	mysqlContainer, err := testcontainers.GenericContainer(ctx, genericContainerReq)
 	if err != nil {
-		t.Fatalf("failed to start container: %s", err)
-	}
-
-	defer func() {
-		if err := testcontainers.TerminateContainer(mysqlContainer); err != nil {
-			t.Logf("failed to terminate container: %v", err)
-		}
-	}()
-
-	if err != nil {
-		t.Fatalf("failed to start MySQL container: %s", err)
+		return nil, fmt.Errorf("failed to start container: %s", err)
 	}
 
 	// コンテナのホストとポートを取得
 	host, err := mysqlContainer.Host(ctx)
 	if err != nil {
-		t.Fatal(err)
+		return nil, fmt.Errorf("failed to get container host: %s", err)
 	}
 
 	port, err := mysqlContainer.MappedPort(ctx, "3306")
 	if err != nil {
-		t.Fatal(err)
+		return nil, fmt.Errorf("failed to get container port: %s", err)
 	}
 
 	// MySQLに接続
 	dsn := fmt.Sprintf("user:password@tcp(%s:%s)/test", host, port.Port())
 
-	db, err := sql.Open("mysql", dsn)
+	mysqlC := &MysqlContainer{
+		mysqlContainer: mysqlContainer,
+		URI:            dsn,
+	}
+
+	return mysqlC, nil
+}
+
+func TestUser_FetchUserByID(t *testing.T) {
+	ctx := context.Background()
+
+	// コンテナ内で実行したいschema.sqlのパスを指定
+	schemaPath, err := filepath.Abs("../models/schema.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Testcontainersを使ってMySQLコンテナを起動させる関数を呼び出しコンテナを起動
+	// 関数の実装については後述
+	mysqlContainer, err := setupMysqlContainerWithModule(ctx, schemaPath)
+	if err != nil {
+		t.Fatalf("failed to start MySQL container: %s", err)
+	}
+
+	// テスト終了後にコンテナをクリーンアップ
+	testcontainers.CleanupContainer(t, mysqlContainer.mysqlContainer)
+
+	db, err := sql.Open("mysql", mysqlContainer.URI)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer db.Close()
 
-	// データベースに接続できるか確認
-	err = db.Ping()
-	if err != nil {
-		t.Fatalf("failed to connect to database: %s", err)
-	} else {
-		t.Log("データベースに接続できました")
-	}
-
 	r := NewUserRepository(db)
 
-	// テストデータを作成
+	// テストケースを作成
 	type args struct {
 		userID string
 	}
@@ -207,6 +161,7 @@ func TestUser_FetchUserByID(t *testing.T) {
 		},
 	}
 
+	// テスト実行
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := r.FetchUserByID(tt.args.userID)
